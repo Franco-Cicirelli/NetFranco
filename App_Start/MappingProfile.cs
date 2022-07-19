397e7dc9e9e956c4ebd1d9bc679a03f86544b1bf
FrancoCicirelliEY <Franco.Cicirelli@gds.ey.com>
2022-07-19 16:59:25 -0300

    Adding an API to de app and improving methods

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NetFranco.Dtos;
+using NetFranco.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NetFranco.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
